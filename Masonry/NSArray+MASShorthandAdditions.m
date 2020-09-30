e03487e09b5f39114dadcecbe8a3235fbe6afea5
xie.longyan <xie.longyan@gloritysolutions.com>
2020-09-30 11:11:30 +0800

    remove project conflicts warnings

---
+++ Masonry/NSArray+MASShorthandAdditions.m
@@ -0,0 +1,24 @@
+//
+//  NSArray+MASShorthandAdditions.m
+//  DemoForDGMasonry
+//
+//  Created by User on 2020/9/17.
+//
+
+#import "NSArray+MASShorthandAdditions.h"
+
+@implementation NSArray (MASShorthandAdditions)
+
+- (NSArray *)makeConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_makeConstraints:block];
+}
+
+- (NSArray *)updateConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_updateConstraints:block];
+}
+
+- (NSArray *)remakeConstraints:(void(^)(MASConstraintMaker *))block {
+    return [self mas_remakeConstraints:block];
+}
+
+@end
